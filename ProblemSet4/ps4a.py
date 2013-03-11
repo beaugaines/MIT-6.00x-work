51e4eb6b747f4562d0d74eb332353b91692d3c03
Charlie Gaines <beaugaines@gmail.com>
2013-03-11 12:05:37 -0400

    refactoring isValidWord

--- ProblemSet4/ps4a.py
+++ ProblemSet4/ps4a.py
@@ -155,8 +155,6 @@ def updateHand(hand, word):
     hand: dictionary (string -> int)    
     returns: dictionary (string -> int)
     """
-    # TO DO ... <-- Remove this comment when you code this function
-
 
     newHand = hand.copy()
     for (k,v) in newHand.items():
