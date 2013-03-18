30484f9f62e2c0a151612be97a01ef050b8dbede
Charlie Gaines <beaugaines@gmail.com>
2013-03-18 17:57:10 -0400

    decrypt story spec passing

--- ProblemSet5/ps5_encryption.py
+++ ProblemSet5/ps5_encryption.py
@@ -187,8 +187,11 @@ def decryptStory():
 
     returns: string - story in plain text
     """
-    ### TODO.
-    return "Not yet implemented." # Remove this comment when you code the function
+    wordlist = loadWords()
+    s = getStoryString()
+    shift = findBestShift(wordlist, s)
+    story = applyShift(s, shift)
+    return story
 
 #
 # Build data structures used for entire session and run encryption
