d4a2a819a5ab0e39f2afe52ae0c7e807bcf60015
Charlie Gaines <beaugaines@gmail.com>
2013-03-19 14:53:41 -0400

    isPrime working

--- ProblemSet5/ps5_recursion.py
+++ ProblemSet5/ps5_recursion.py
@@ -41,8 +41,26 @@ def x_ian(x, word):
     word: a string
     returns: True if word is x_ian, False otherwise
     """
-    for c in x:
-        c in word
+    # base case of empty string
+    if x == "":
+        return True
+
+    # case of x being longer than word
+    if len(x) > len(word):
+        return False
+
+    # case of x and word being identical
+    if x == word:
+        return True
+
+    # check to see if first chars of x and word are same
+    elif(x[0] == word[0]):
+        return x_ian(x[1:], word[1:])
+
+    # otherwise check the next character in word for a match
+    else:
+        return x_ian(x, word[1:])
+
 
 #
 # Problem 5: Typewriter
@@ -58,4 +76,23 @@ def insertNewlines(text, lineLength):
         the next word.
     returns: a string, with newline characters inserted appropriately. 
     """
-    ### TODO.
+
+    wordList = text.split()
+    textLength = len(text)
+
+    textCopy = text[:]
+    newString = ''
+    tempString = ''
+
+    while len(wordList) > 0:
+        tempWord = wordList.unshift()
+        while len(tempString) < lineLength:
+            newString += tempWord + ' '
+        newString += tempString + '.\n'
+    return newString
+
+
+
+
+
+
