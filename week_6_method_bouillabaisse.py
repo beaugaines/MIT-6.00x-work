98050faa319a33984719e48089326a13304eff0b
Charlie Gaines <beaugaines@gmail.com>
2013-03-29 15:23:16 -0400

    added hashValue and member methods to HashSet

--- week_6_method_bouillabaisse.py
+++ week_6_method_bouillabaisse.py
@@ -8,6 +8,46 @@ class HashSet(object):
     hash fcn i.e. dictionaries - but in much less efficient fashion
     '''
 
+    def __init__(self, numBuckets):
+        '''
+        numBuckets:  int.  The number of buckets this hash set will have.  Raises
+        ValueError if this value is not an integer, or if it is not greater
+        than zero.
+        
+        Sets up an empty hash set with numBuckets number of buckets.
+        '''
+        if type(numBuckets) != int or numBuckets <= 0:
+            raise ValueError
+        else:
+            hashSet = []
+            for i in range(0, numBuckets):
+                hashSet.append([])
+
+        def hashValue(self, e):
+        '''
+        e: an integer
+        
+        returns: a hash value for e, which is e mod the number 'o buckets in
+        this hash set.  Raise ValueError if e is not an int.
+        '''
+            if type(e) != int:
+                raise ValueError
+            else:
+                return e % len(hashSet)
+
+        def member(self, e):
+            '''
+            e: an integer
+        
+            returns: True if e is in self, False otherwise
+            Raise ValueError if e not an integer
+            '''
+            if type(e) != int:
+                raise ValueError
+            else:
+                return e in self.hashSet
+
+
 
 
 # primeGen generator fcn
