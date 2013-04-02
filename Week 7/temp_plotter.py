56bd7eb54740bbebbcdb8d0b37c3e1de9c51316c
Charlie Gaines <beaugaines@gmail.com>
2013-04-02 16:10:36 -0400

    pyplot looking pretty good

--- Week 7/temp_plotter.py
+++ Week 7/temp_plotter.py
@@ -1,3 +1,5 @@
+import pylab
+
 
 """ simple fcn to parse a text file of temperature data and print out
     the daily high-low differential using pyplot
@@ -9,7 +11,8 @@ lows = []
 
 allTemps = []
 
-#read in the file
+#read in the file, split the lines, and push the relevant data onto the high/low arrays
+
 with open('julyTemps.txt') as f:
   for line in f.readlines():
     fields = line.split()
@@ -18,6 +21,22 @@ with open('julyTemps.txt') as f:
     else:
       highs.append(int(fields[1]))
       lows.append(int(fields[2]))
+
+
+# calculate daily temp differentials
+
+diffTemps = []
+for i in range(len(highs)):
+    diffTemps.append(highs[i] - lows[i])
+
+
+# plot it
+pylab.plot(range(1,32), diffTemps)
+pylab.title('Day by Day Ranges in Temperature in Boston in July 2012')
+pylab.xlabel('Days')
+pylab.ylabel('Temperature Ranges')
+pylab.show()
+
 
 
 
