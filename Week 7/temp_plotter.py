d76d4b9be6c6407bf8a05b1cfc5267dd84a93d4c
Charlie Gaines <beaugaines@gmail.com>
2013-04-02 15:55:27 -0400

    refactored loop to split highs and lows into respective lists

--- Week 7/temp_plotter.py
+++ Week 7/temp_plotter.py
@@ -12,39 +12,12 @@ allTemps = []
 #read in the file
 with open('julyTemps.txt') as f:
   for line in f.readlines():
-    if len(line) < 3 or not line[0].isdigit():
+    fields = line.split()
+    if len(fields) < 3 or not fields[0].isdigit():
       pass
     else:
-      allTemps.append(line.split())
+      highs.append(fields[1])
+      lows.append(fields[2])
 
 
-# inFile = open('julyTemps.txt')
 
-# for line in inFile:
-#   fields = line.split()
-#   if len(fields) < 3 or not fields[0].isdigit():
-#     pass
-#   else:
-#     allTemps.append(fields)
-
-for f in allTemps:
-  print f
-
-# for f in fields:
-#   if len(f) < 3 or not f[0].isdigit() or f[0] == '-':
-#     fields.remove(f)
-#   else:
-#     continue
-
-
-# for f in fields:
-#   print f
-
-
-# for field in fields:
-#   if len(field) < 3 or not field[0].isdigit():
-#     fields.remove(field)
-
-
-# for f in fields:
-#   print f
