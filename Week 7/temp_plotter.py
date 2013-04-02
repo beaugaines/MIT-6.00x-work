521a7e38b3e5f107cb7c3ff6de14e060e2cf8d95
Charlie Gaines <beaugaines@gmail.com>
2013-04-02 15:50:38 -0400

    file process in the loop working

--- Week 7/temp_plotter.py
+++ Week 7/temp_plotter.py
@@ -1,5 +1,50 @@
-import pyplot
 
-inFile = open('julyTemps.txt', 'r')
+""" simple fcn to parse a text file of temperature data and print out
+    the daily high-low differential using pyplot
+"""
 
-lines
+# initialize our storage
+highs = []
+lows = []
+
+allTemps = []
+
+#read in the file
+with open('julyTemps.txt') as f:
+  for line in f.readlines():
+    if len(line) < 3 or not line[0].isdigit():
+      pass
+    else:
+      allTemps.append(line.split())
+
+
+# inFile = open('julyTemps.txt')
+
+# for line in inFile:
+#   fields = line.split()
+#   if len(fields) < 3 or not fields[0].isdigit():
+#     pass
+#   else:
+#     allTemps.append(fields)
+
+for f in allTemps:
+  print f
+
+# for f in fields:
+#   if len(f) < 3 or not f[0].isdigit() or f[0] == '-':
+#     fields.remove(f)
+#   else:
+#     continue
+
+
+# for f in fields:
+#   print f
+
+
+# for field in fields:
+#   if len(field) < 3 or not field[0].isdigit():
+#     fields.remove(field)
+
+
+# for f in fields:
+#   print f
