cec3ffba8d08741147dcd879d9492c9c398c1c3e
Charlie Gaines <beaugaines@gmail.com>
2013-04-03 15:05:24 -0400

    pal_2 tests passing

--- utoronto_course.py
+++ utoronto_course.py
@@ -1,14 +1,37 @@
 # more palindromes!
 
+import unittest
+
 def pal_1(s):
-  """
-  (str) -> bool
-  Return True if an only if s is a palindrome
-  pal_1('noon')  ->  True
-  pal_1('fugue')  -> False
-  """
-  return reverse(s) == s
+    """
+    (str) -> bool
+    Return True if an only if s is a palindrome
+    pal_1('noon')  ->  True
+    pal_1('fugue')  -> False
+    """
+    return reverse(s) == s
   
+
+def pal_2(s):
+    """
+    (str) -> bool
+    Return True if an only if s is a palindrome
+    pal_1('noon')  ->  True
+    pal_1('fugue')  -> False
+    """
+    if type(s) != str:
+        raise ValueError('Must enter a string.')
+    if len(s) == 0:
+        return False
+    if len(s) == 1:
+        return True
+    divisor = len(s)//2
+    if divisor % 2 == 1:
+        return s[:divisor] == s[divisor+1:]
+    else:
+        return s[:divisor] == s[divisor:]
+
+      
 
 def reverse(s):
     """
@@ -36,8 +59,19 @@ def test(got,expected):
     print "{0} got: {1}, expected {2}".format(prefix, repr(got), repr(expected))
 
 
-# test pal_1
 
+
+# test pal_1
+print 'test pal_1'
+print '----------'
 test(pal_1('bob'), True)
 test(pal_1('a'), True)
 test(pal_1('fugue'), False)
+
+# test pal_2
+print 'test pal_2'
+print '----------'
+test(pal_2('bob'), True)
+test(pal_2('a'), True)
+test(pal_2('fugue'), False)
+test(pal_2(''), False)
