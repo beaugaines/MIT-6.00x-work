33c3dfbf7a967a857dd08c6b501d180d058f597b
Charlie Gaines <beaugaines@gmail.com>
2013-04-03 14:08:33 -0400

    added pal1 and test fcn

--- utoronto_course.py
+++ utoronto_course.py
@@ -1,3 +1,36 @@
 # more palindromes!
 
-def 
+def pal_1(s):
+  """
+  (str) -> bool
+  Return True if an only if s is a palindrome
+  pal_1('noon')  ->  True
+  pal_1('fugue')  -> False
+  """
+  return reverse(s) == s
+  
+
+def reverse(s):
+    """
+    (str) -> str
+    Return a reversed version of s
+    >>> pal_1('hello')
+    'olleh'
+    >>> pal_1('a')
+    'a'
+    """
+
+    rev = ''
+    # for each char in s, add each char to the beginning of rev
+    for c in s:
+        rev = c + rev
+    return rev
+
+# tests!
+
+def test(got,expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = ' FAIL '
+    print "{0} got: {1}, expected {2}".format(prefix, repr(got), repr(expected))
