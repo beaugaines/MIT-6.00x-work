24b7413f67bc3b837ac2721f290e41f1e52304a5
Charlie Gaines <beaugaines@gmail.com>
2013-04-03 12:14:58 -0400

    added utoronto file, might need its own repo, we shall see

--- GoogleExercises/list1.py
+++ GoogleExercises/list1.py
@@ -62,9 +62,7 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-  # +++your code here+++
-  return
-
+    
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
