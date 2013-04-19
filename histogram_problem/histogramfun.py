417439282f7f1c12328bf2d54dfcbccebb6812a5
Charlie Gaines <beaugaines@gmail.com>
2013-04-19 16:48:51 -0400

    histogram problem

---
+++ histogram_problem/histogramfun.py
@@ -0,0 +1,43 @@
+import pylab
+
+
+WORDLIST_FILENAME = "words.txt"
+
+def loadWords():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    # wordList: list of strings
+    wordList = []
+    for line in inFile:
+        wordList.append(line.strip().lower())
+    print "  ", len(wordList), "words loaded."
+    return wordList
+
+def plotVowelProportionHistogram(wordList, numBins=15):
+    """
+    Plots a histogram of the proportion of vowels in each word in wordList
+    using the specified number of bins in numBins
+    """
+    VOWELS = 'aeiou'
+    freq = {}
+    vowel_proportions = []
+    for w in wordList:
+        num_vowels = 0
+        for c in w:
+            if c in VOWELS:
+                num_vowels += 1
+        vowel_proportions.append(num_vowels/float(len(w)))
+    mean = sum(total)/float(len(total))
+    # plot histogram with pyab
+    pylab.hist()
+
+if __name__ == '__main__':
+    wordList = loadWords()
+    plotVowelProportionHistogram(wordList)
---
+++ web_scraper.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+
+BASE_URL = "http://www.chicagoreader.com"
+
+def get_category_links(section_url):
+    html = urlopen(section_url).read()
+    soup = BeautifulSoup(html, 'lxml')
+    boccat = soup.find('dl', 'boccat')
+    category_links = [BASE_URL + dd.a['href'] for dd in boccat.findAll('dd')]
+    return category_links
+
+def get_category_winner(category_url):
+    html = urlopen(category_url).read()
+    
