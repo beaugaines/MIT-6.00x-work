0618b23f0263ad80fcb970e704847f24422dee1f
Charlie Gaines <beaugaines@gmail.com>
2013-04-19 16:49:42 -0400

    wb scper

--- web_scraper.py
+++ web_scraper.py
@@ -5,6 +5,7 @@ BASE_URL = "http://www.chicagoreader.com"
 
 def get_category_links(section_url):
     html = urlopen(section_url).read()
+    # maybe use HTML5 parser instead?
     soup = BeautifulSoup(html, 'lxml')
     boccat = soup.find('dl', 'boccat')
     category_links = [BASE_URL + dd.a['href'] for dd in boccat.findAll('dd')]
@@ -12,4 +13,4 @@ def get_category_links(section_url):
 
 def get_category_winner(category_url):
     html = urlopen(category_url).read()
-    
+        
