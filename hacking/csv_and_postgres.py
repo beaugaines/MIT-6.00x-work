72bcff5fb0329cd39cf4bf32ee6e6b04f6eb6150
Charlie Gaines <beaugaines@gmail.com>
2013-05-09 20:19:43 -0400

    csv munging coming along nicely

--- hacking/csv_and_postgres.py
+++ hacking/csv_and_postgres.py
@@ -1,39 +1,72 @@
 import csv
 import psycopg2 as psycho
 import sys
+import re
 
-DB = 'nyc-wifi.db'
 con = None
 
 # munge the file!
 with open('../nyc_addresses.csv', 'rb') as infile:
     addresses = list(csv.reader(infile))
 
-print addresses[1]
+res = ()
 
-try:
-    con = psycho.connect(database='playground', user='sircharles')
-    cur = con.cursor()
+lat, long = addresses[1][1].split()
 
-    # create table
-    cur.execute("""
+# expunge punctuation
+lat = re.sub('[(),"]', '', lat)
+long = re.sub('[(),"]', '', long)
 
-        CREATE TABLE wifi_hotspots(id INT PRIMARY KEY,
-                                    latitude FLOAT8,
-                                    longitude FLOAT8,
-                                    zip INT,
-                                    phone INT,
-                                    free BOOL,
-                                    url varchar(256)
+address = addresses[1][2].split()
+street_num = address[0]
+street = address[1] + ' ' + address[2]
+city = addresses[1][5]
+zip = addresses[1][6]
+print addresses[1][6]
 
-    """)
 
-except psycho.DatabaseError, e:
-    print 'Error {0}'.format(e)
-    sys.exit(1)
 
-finally:
-    if con:
-        con.close()
+# try:
+#     con = psycho.connect(database='playground', user='sircharles')
+#     cur = con.cursor()
+
+#     # create table
+#     cur.execute("""
+
+#         CREATE TABLE wifi_hotspots(id INT PRIMARY KEY,
+#                                     latitude FLOAT8,
+#                                     longitude FLOAT8,
+#                                     zip INT,
+#                                     phone INT,
+#                                     free BOOL,
+#                                     url varchar(256),
+#                                     city varchar(20),
+#                                     street varchar(30),
+#                                     street_number INT,
+#     """)
+
+#     # insert records
+#     query = """
+#             INSERT INTO 
+#                 wifi_hotspots
+#                 (id, latitude, longitude, zip, phone,
+#                 free, url, city, street, street_number)
+#             VALUES
+#                 (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+#             """
+
+#     for location in addresses:
+#         cur.execute("""
+#             INSERT INTO wifi_hotspots
+#             VALUES 
+#             """)
+
+# except psycho.DatabaseError, e:
+#     print 'Error {0}'.format(e)
+#     sys.exit(1)
+
+# finally:
+#     if con:
+#         con.close()
 
 
