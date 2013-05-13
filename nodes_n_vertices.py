693f89b9120e5c77b7bc0c5b50d7657397d373c9
Charlie Gaines <beaugaines@gmail.com>
2013-05-12 22:50:22 -0400

    graph class

--- nodes_n_vertices.py
+++ nodes_n_vertices.py
@@ -41,5 +41,46 @@ class WeightedEdge(Edge):
           + str(self.dest)
 
 
+class Digraph(object):
+    """return a directional graph object"""
+    def __init__(self):
+        self.nodes = set([])
+        self.edges = {}
+
+    def addNode(self, node):
+        if node in self.nodes:
+            raise ValueError('Dup node')
+        else:
+            self.nodes.add(node)
+            self.edges[node] = []
+
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getEdge()
+        if not(src in self.nodes and dest in self.nodes):
+            raise ValueError("Node not in graph")
+        self.edges[src].append(dest)
+
+    def childrenOf(self, node):
+        return self.edges[node]
+
+    def hasNode(self, node):
+        return node in self.nodes
+
+    def __str__(self):
+        res = ''
+        for k in self.edges:
+            for d in self.edges[k]:
+                res += str(k) + '->' + str(d) + '\n'
+
+        return res[:-1]
+
+class Graph(Digraph):
+        
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+        
         
         
